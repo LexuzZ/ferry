ea511060986c1754ae4c9f019ba8a75951387f67
Haerul Taka <haerultaka255@gmail.com>
2024-05-05 23:28:39 +0700

    seeder data jadwal

--- database/seeders/JadwalSeeder.php
+++ database/seeders/JadwalSeeder.php
@@ -19,6 +19,7 @@ class JadwalSeeder extends Seeder
             Jadwal::create([
                  // ID kapal akan dipilih secara acak antara 1 dan 10
                  'rute_id' => rand(1, 4),
+                 'kapal_id' => rand(1, 4),
                 'tanggal' => Carbon::now()->addDays($i)->format('Y-m-d'), // ID kapal akan dipilih secara acak antara 1 dan 10
                 'tiba' => now()->addDays($i)->setHour(rand(8, 12))->setMinute(rand(0, 59)), // Waktu berangkat akan ditetapkan ke hari-hari berikutnya dengan jam acak antara 8 dan 12
                 'keberangkatan' => now()->addDays($i)->setHour(rand(13, 18))->setMinute(rand(0, 59)), // Waktu sampai akan ditetapkan ke hari-hari berikutnya dengan jam acak antara 13 dan 18
