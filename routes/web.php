83ebfab11618349444ff450c047445207eade559
Haerul Taka <haerultaka255@gmail.com>
2024-05-17 22:59:10 +0700

    create routing order

--- routes/web.php
+++ routes/web.php
@@ -64,8 +64,9 @@ Route::group(['middleware' => 'auth'], function () {
         Route::inertia('/userDashboard', 'UserDashboard')->name('userDashboard');
         Route::get('/userJadwal/{ruteId}', [JadwalUserController::class, 'show'])->name('jadwaluser.show');
         Route::get('/userJadwal/{id}/detail', [RuteController::class, 'show'])->name('rute.show');
+        Route::get('/order/{id}', [JadwalUserController::class, 'order'])->name('order');
         Route::get('/userJadwal', [JadwalUserController::class, 'index'])->name('jadwal.user');
-        Route::get('/pesanan', [JadwalUserController::class, 'index'])->name('jadwal.pesanan');
+        // Route::get('/pesanan', [JadwalUserController::class, 'index'])->name('jadwal.pesanan');
         Route::get('/seats/{kapal}', [SeatController::class, 'index'])->name('seats.index');
         Route::post('/seats/{seat}/reserve', [SeatController::class, 'reserve'])->name('seats.reserve');
     });
