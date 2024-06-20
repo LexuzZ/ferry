fe23944df292df64c2dd444c6a90c11d9c46523f
Haerul Taka <haerultaka255@gmail.com>
2024-06-20 19:41:31 +0700

    modify route order

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('welcome', function () {
 Route::get('/', [HomeController::class, 'index']);
 Route::get('{id}/detail', [HomeController::class, 'show']);
 // Route::get('/', [HomeController::class, 'userbooking'])->name('home.userbooking');
-
+Route::post('payment/callback', [PaymentController::class, 'store'])->name('payment.callback');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get("/redirectAuthenticatedUsers", [RedirectAuthenticatedUsersController::class, "home"])->name('redirectAuthenticatedUsers');
@@ -76,7 +76,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('payment/{ticket}', [PaymentController::class, 'create'])->name('payment.create');
 
         // Rute untuk menangani callback dari Midtrans
-        Route::post('payment/callback', [PaymentController::class, 'store'])->name('payment.callback');
+        
     });
 });
 
