073fe7bc2221d8862f7b4c175458ba50d8308005
Haerul Taka <haerultaka255@gmail.com>
2024-06-27 22:06:53 +0700

    modify component seats

--- resources/js/Components/SeatSelection.jsx
+++ resources/js/Components/SeatSelection.jsx
@@ -1,25 +1,31 @@
-// resources/js/Components/SeatSelection.js
+// resources/js/Pages/Seats/Index.jsx
 
-import React from "react";
-import { Inertia } from "@inertiajs/inertia";
-import UserLayout from "@/Layouts/UserLayout";
+import React, { useState } from 'react';
+import { Inertia } from '@inertiajs/inertia';
+import UserLayout from '@/Layouts/UserLayout';
+import { Link } from '@inertiajs/react';
 
-const SeatSelection = ({ seats, kapal_id }) => {
+const SeatSelection = ({ seats, kapal_id, jadwal_id }) => {
     const reserveSeat = (seatId) => {
         if (confirm("Apakah Anda yakin ingin memesan tempat duduk ini?")) {
-            Inertia.post(route("seats.reserve", seatId));
+            Inertia.post(route("seats.reserve", { seat: seatId }), { kapal_id, jadwal_id });
         }
     };
 
     return (
         <UserLayout>
             <div>
-                <h2 className="pt-20 mb-5 font-bold text-center text-xl text-midnight">
+                <h2 className="pt-20 mb-5 font-bold text-center text-xl text-midnight ">
                     Pilih Tempat Duduk
                 </h2>
-                <div className="grid grid-cols-5 gap-5">
+                <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-1 gap-2 px-4">
                     {seats.map((seat) => (
-                        <div key={seat.id} className="p-5 flex justify-center items-center rounded-lg max-w-xs shadow-lg text-midnight">
+                        <div
+                            key={seat.id}
+                            className={`p-5 flex justify-center items-center rounded-lg max-w-xs shadow-lg text-midnight cursor-pointer ${
+                                seat.available ? "bg-green" : "bg-red"
+                            }`}
+                        >
                             No. Seat : {seat.name}
                             {!seat.available ? (
                                 <span className="ml-4 text-red-500">Tidak Tersedia</span>
@@ -33,6 +39,7 @@ const SeatSelection = ({ seats, kapal_id }) => {
                             )}
                         </div>
                     ))}
+                    <Link href='/order'>Kembali</Link>
                 </div>
             </div>
         </UserLayout>
