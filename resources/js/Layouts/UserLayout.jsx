d0555ede11bdf67ac0e2265385316de43a5b9eb3
Haerul Taka <haerultaka255@gmail.com>
2024-07-22 19:48:07 +0700

    mobile first

--- resources/js/Layouts/UserLayout.jsx
+++ resources/js/Layouts/UserLayout.jsx
@@ -31,47 +31,63 @@ export default function UserLayout({ children }) {
                 <nav className="fixed top-0 z-50 w-full  border-b border-gray-200 bg-white dark:border-gray-700">
                     <div className="px-3 py-3 lg:px-5 lg:pl-3">
                         <div className="flex items-center justify-between">
-                            <div className="flex items-center justify-start rtl:justify-end">
-                                {/* <button
-                                    data-drawer-target="logo-sidebar"
-                                    data-drawer-toggle="logo-sidebar"
-                                    aria-controls="logo-sidebar"
-                                    type="button"
-                                    className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                                >
-                                    <span className="sr-only">
-                                        Open sidebar
-                                    </span>
-                                    <svg
-                                        className="w-6 h-6"
-                                        aria-hidden="true"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            clipRule="evenodd"
-                                            fillRule="evenodd"
-                                            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                                        />
-                                    </svg>
-                                </button> */}
-                                <details className="sm:hidden dropdown bg-grey">
-                                    <summary className="btn m-1">Menu</summary>
-                                    <ul className=" menu dropdown-content bg-grey rounded-box z-[1] w-52 p-2 shadow">
+                            <div className="flex items-center justify-between rtl:justify-end">
+                                <details className="sm:hidden dropdown">
+                                    <summary className="btn m-1 text-white">
+                                        Menu
+                                    </summary>
+                                    <ul className="menu dropdown-content bg-white text-midnight rounded-box z-[1] w-52 p-2 shadow">
                                         <li>
-                                            <a>Item 1</a>
+                                            <Link href={route("userDashboard")}>
+                                                Home
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a>Item 2</a>
+                                            <Link
+                                                href={route("jadwal.riwayat")}
+                                            >
+                                                Pesanan Saya
+                                            </Link>
                                         </li>
                                     </ul>
                                 </details>
+                                <div className="sm:hidden ml-48 w-full sm:w-auto">
+                                    <Dropdown>
+                                        <Dropdown.Trigger>
+                                            <span className="inline-flex rounded-md dropdown dropdown-end">
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-midnight  hover:bg-gray hover:text-white focus:outline-none transition ease-in-out duration-150"
+                                                >
+                                                    <span className="px-2">
+                                                        {auth.user.name}
+                                                    </span>
+                                                </button>
+                                            </span>
+                                        </Dropdown.Trigger>
+
+                                        <Dropdown.Content>
+                                            <Dropdown.Link
+                                                href={route("profile.edit")}
+                                            >
+                                                Profile
+                                            </Dropdown.Link>
+                                            <Dropdown.Link
+                                                href={route("logout")}
+                                                method="post"
+                                                as="button"
+                                            >
+                                                Log Out
+                                            </Dropdown.Link>
+                                        </Dropdown.Content>
+                                    </Dropdown>
+                                </div>
+
                                 <a
                                     href="/"
                                     className="ms-2 md:me-24 hidden md:block"
                                 >
-                                    <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-midnight ">
+                                    <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-midnight">
                                         DLNFerry
                                     </span>
                                 </a>
@@ -80,7 +96,7 @@ export default function UserLayout({ children }) {
                                     active={route().current("userDashboard")}
                                     className="me-4"
                                 >
-                                    Dashboard
+                                    Home
                                 </NavLink>
                                 <NavLink
                                     href={route("jadwal.riwayat")}
@@ -96,7 +112,6 @@ export default function UserLayout({ children }) {
                                     Riwayat Pesanan
                                 </NavLink>
                             </div>
-                            <div className="flex items-center me-24 "></div>
 
                             <div className="hidden sm:flex sm:items-center sm:ms-6">
                                 <div className="ms-3 relative">
