2ea2b71ac94e9df263432400a5df60595182c2dc
Haerul Taka <haerultaka255@gmail.com>
2024-07-24 01:05:21 +0700

    modify user layout

--- resources/js/Layouts/UserLayout.jsx
+++ resources/js/Layouts/UserLayout.jsx
@@ -28,7 +28,7 @@ export default function UserLayout({ children }) {
     return (
         <>
             <div className="min-h-screen bg-grey">
-                <nav className="fixed top-0 z-50 w-full  border-b border-gray-200 bg-white dark:border-gray-700">
+                <nav className="fixed top-0 z-50 w-full   border-b border-gray-200 bg-white dark:border-gray-700">
                     <div className="px-3 py-3 lg:px-5 lg:pl-3">
                         <div className="flex items-center justify-between">
                             <div className="flex items-center justify-between rtl:justify-end">
